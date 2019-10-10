cdee88adba48c7b7ebd1d5f682115ee1c4104791
chinedum onyema <onyemachinedum@gmail.com>
2019-10-10 10:02:30 +0100

    <feat> create middleware for the POST route of API/recipes endpoint and log the data to console ntil database is setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 //  enable cross origin resource sharing (CORS)
 app.use((req, res, next) => {
@@ -8,6 +11,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/recipes', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'recipe created successfully!'
+    });
 });
 
 //  get all recipes
