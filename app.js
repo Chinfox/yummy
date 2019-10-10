522ff1f0828874c02f4448baedd14b74a571a4b1
chinedum onyema <onyemachinedum@gmail.com>
2019-10-10 20:11:50 +0100

    <fix> ensure that user app is effectively GET all recipes from the database and that user can POST new recipes to the database

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Recipe = require('./models/recipes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://owi:KW1vE2szoTp0EeGY@cluster0-7cerh.mongodb.net/admin?retryWrites=true&w=majority')
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://owiwi:b4CG56I70A68QaoP@cluster0-7cerh.mongodb.net/uba?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then(() => {
         console.log('Database connection successful');
     })
-    .catch((err) => {
-        console.log(`${err}: Failed to connect to Database`);
+    .catch((error) => {
+        console.log(`${error}: Failed to connect to Database`);
     });
 
 app.use(bodyParser.json());
@@ -24,33 +27,62 @@ app.use((req, res, next) => {
 
 //  POST a recipe
 app.post('/api/recipes', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'recipe created successfully!'
+    // console.log(req.body);
+    // res.status(201).json({
+    //     message: 'recipe created successfully!'
+    // });
+
+    const recipe = new Recipe({
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        time: req.body.time,
+        difficulty: req.body.difficulty
     });
+
+		recipe.save()
+			.then(() => {
+				res.status(201).json({
+					message: 'Recipe saved successfully'
+				});
+			})
+			.catch((error) => {
+				res.status(400).json({
+					error: error
+				});
+			});
 });
 
 //  GET all recipes
 app.use('/api/recipes', (req, res, next) => {
-    const recipeSamples = [
-        {
-            _id: '1',
-            title: 'my title',
-            ingredients: 'pepper',
-            instructions: 'cook',
-            time: '10',
-            difficulty: '5'
-        },
-        {
-            _id: '2',
-            title: 'my second',
-            ingredients: 'onion, tomato',
-            instructions: 'boil, season',
-            time: '23',
-            difficulty: '2'
-        }
-    ];
-    res.status(200).json(recipeSamples);
+    // const recipeSamples = [
+    //     {
+    //         _id: '1',
+    //         title: 'my title',
+    //         ingredients: 'pepper',
+    //         instructions: 'cook',
+    //         time: '10',
+    //         difficulty: '5'
+    //     },
+    //     {
+    //         _id: '2',
+    //         title: 'my second',
+    //         ingredients: 'onion, tomato',
+    //         instructions: 'boil, season',
+    //         time: '23',
+    //         difficulty: '2'
+    //     }
+    // ];
+		// res.status(200).json(recipeSamples);
+		Recipe.find()
+			.then((recipes) => {
+				res.status(200).json(recipes);
+			})
+			.catch((error) => {
+				res.status(400).json({
+					error: error
+				});
+			});
 });
 
 module.exports = app;
--- models/recipes.js
+++ models/recipes.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const recipeSchema = mongoose.Schema({
     title: { type: String, required: true },
     ingredients: { type: String, required: true },
-    instruction: { type: String, required: true },
+    instructions: { type: String, required: true },
     time: { type: Number, required: true },
     difficulty: { type: Number, required: true },
 });
