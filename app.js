66d9bbbef4622a1f64460923d267f09e08d11079
chinedum onyema <onyemachinedum@gmail.com>
2019-10-10 09:54:13 +0100

    <feat> create middleware for the GET route of API/recipe endpoint with dummy recipe data

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ const express = require('express');
 
 const app = express();
 
+//  enable cross origin resource sharing (CORS)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-app.use((req, res) => {
-    res.json({message: 'Express is listening!'});
+
+//  get all recipes
+app.use('/api/recipes', (req, res, next) => {
+    const recipeSamples = [
+        {
+            _id: '1',
+            title: 'my title',
+            ingredients: 'pepper',
+            instructions: 'cook',
+            time: '10',
+            difficulty: '5'
+        },
+        {
+            _id: '2',
+            title: 'my second',
+            ingredients: 'onion, tomato',
+            instructions: 'boil, season',
+            time: '23',
+            difficulty: '2'
+        }
+    ];
+    res.status(200).json(recipeSamples);
 });
 
 module.exports = app;
